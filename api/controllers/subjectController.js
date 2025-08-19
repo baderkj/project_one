b5751b10abc2eb42266ad95e6f65bc3668360039
Mo-Ha-0 <hamad.mohamad.2004@gmail.com>
2025-08-19 21:31:39 +0300

    create subject get level_grade instead of curriculum_id

--- api/controllers/subjectController.js
+++ api/controllers/subjectController.js
@@ -5,92 +5,108 @@ const { validationResult } = require('express-validator');
 
 const bcrypt = require('bcrypt-nodejs');
 module.exports = {
-  async createSubject(req, res) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      const Subject = await subjectService.createSubject(req.body);
-      res.status(201).json(Subject);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  },
+    async createSubject(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const { level_grade } = req.body;
+            const curriculum = await db('curriculums')
+                .select('*')
+                .where({ level_grade: String(level_grade), is_active: true })
+                .first();
+            if (!curriculum) {
+                return res.status(404).json({
+                    error: 'Curriculum not found for provided level_grade',
+                });
+            }
 
-  async getSubject(req, res) {
-    try {
-      const Subject = await subjectService.getSubject(req.params.id);
-      if (!Subject) return res.status(404).json({ error: 'Subject not found' });
-      res.json(Subject);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+            req.body.curriculum_id = curriculum.id;
+            delete req.body.level_grade;
 
-  async getAllSubjectes(req, res) {
-    try {
-      const Subject = await subjectService.getAllSubjectes();
-      res.json(Subject);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+            const Subject = await subjectService.createSubject(req.body);
+            res.status(201).json(Subject);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    },
 
-  async getAllSubjectesOfStudent(req, res) {
-    const userId = req.user.id;
-    try {
-      const studentCurriculum = await db('students')
-        .select('curriculum_id')
-        .where({ user_id: userId })
-        .first();
+    async getSubject(req, res) {
+        try {
+            const Subject = await subjectService.getSubject(req.params.id);
+            if (!Subject)
+                return res.status(404).json({ error: 'Subject not found' });
+            res.json(Subject);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-      if (!studentCurriculum) {
-        return res
-          .status(404)
-          .json({ error: 'Student record not found for this user' });
-      }
-      const subjects = await db('subjects')
-        .select('*')
-        .where({ curriculum_id: studentCurriculum.curriculum_id});
+    async getAllSubjectes(req, res) {
+        try {
+            const Subject = await subjectService.getAllSubjectes();
+            res.json(Subject);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-      res.json(subjects);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+    async getAllSubjectesOfStudent(req, res) {
+        const userId = req.user.id;
+        try {
+            const studentCurriculum = await db('students')
+                .select('curriculum_id')
+                .where({ user_id: userId })
+                .first();
 
-  async updateSubject(req, res) {
-    try {
-      const Subject = await subjectService.updateSubject(
-        req.params.id,
-        req.body
-      );
-      if (!Subject || Subject.length == 0)
-        return res.status(404).json({ error: 'Subject not found' });
-      res.json(Subject);
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  },
+            if (!studentCurriculum) {
+                return res
+                    .status(404)
+                    .json({ error: 'Student record not found for this user' });
+            }
+            const subjects = await db('subjects')
+                .select('*')
+                .where({ curriculum_id: studentCurriculum.curriculum_id });
 
-  async deleteSubject(req, res) {
-    try {
-      const result = await subjectService.deleteSubject(req.params.id);
-      if (!result) return res.status(404).json({ error: 'Subject not found' });
-      res.status(200).json({ message: 'deleted successfuly' });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+            res.json(subjects);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 
-  async getSubjectsList(req, res) {
-    try {
-      const Subjects = await subjectService.getAllSubjectsNames();
+    async updateSubject(req, res) {
+        try {
+            const Subject = await subjectService.updateSubject(
+                req.params.id,
+                req.body
+            );
+            if (!Subject || Subject.length == 0)
+                return res.status(404).json({ error: 'Subject not found' });
+            res.json(Subject);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
+    },
 
-      res.json(Subjects);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  },
+    async deleteSubject(req, res) {
+        try {
+            const result = await subjectService.deleteSubject(req.params.id);
+            if (!result)
+                return res.status(404).json({ error: 'Subject not found' });
+            res.status(200).json({ message: 'deleted successfuly' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
+
+    async getSubjectsList(req, res) {
+        try {
+            const Subjects = await subjectService.getAllSubjectsNames();
+
+            res.json(Subjects);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    },
 };
--- api/validators/subjectValidator.js
+++ api/validators/subjectValidator.js
@@ -4,7 +4,7 @@ exports.subjectValidator = [
     body('name').isString({ min: 3 }).withMessage('Invalid class_name'),
     body('resources').isString({ min: 3 }).withMessage('Invalid resources'),
     // body('teacher_id').isInt({min:1}).withMessage('Invalid teacher_id'),
-    body('curriculum_id')
-        .isInt({ min: 1 })
-        .withMessage('Invalid curriculum_id'),
+    body('level_grade')
+        .isInt({ min: 9, max: 12 })
+        .withMessage('level_grade must be one of [9, 10, 11, 12]'),
 ];
