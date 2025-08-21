56bb25041bb391db399729866b53bdbc88346e6b
Mo-Ha-0 <hamad.mohamad.2004@gmail.com>
2025-08-21 16:15:52 +0300

    fixed returning data for get teacher's students

--- firebase/firebase-admin.js
+++ firebase/firebase-admin.js
@@ -2,10 +2,10 @@ const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK
 
-const serviceAccount = require('./project-one-a5b2f-firebase-adminsdk-fbsvc-4779cc16a1.json');
+// const serviceAccount = require('./project-one-a5b2f-firebase-adminsdk-fbsvc-4779cc16a1.json');
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    // credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
